0293f5503226066aa12275143881a6fbab489ef5
tkk21 <tkk21@case.edu>
2015-10-31 23:52:43 -0400

    added fucntion for initializing semaphores

--- main.c
+++ main.c
@@ -9,6 +9,8 @@ typedef struct _thread_data_ {
     int readcount; // status of whether or not something was read or not
 } thread_data;
 
+sem_t mutex, wrt;
+
 // Error checked syscall of semaphore wait
 void wait(sem_t *sem){
     if(sem_wait(sem) < 0) {
@@ -22,6 +24,27 @@ void signal(sem_t *sem){
     if(sem_post(sem) < 0){
         perror ("sem_post");
         exit (EXIT_FAILURE);
+    }
+}
+
+void *read(void *args){
+    thread_data *data = (thread_data *)args;
+
+}
+
+void *write(void *args){
+    thread_data *data = (thread_data *)args;
+}
+
+// Initializing the semaphores
+void initialize_semaphores(){
+    if (sem_init(&mutex, 0, (unsigned int)1)<0){
+        perror("sem_init(mutex)");
+        exit(EXIT_FAILURE);
+    }
+    if (sem_init(&wrt, 0, (unsigned int)1)<0){
+        perror("sem_init(wrt)");
+        exit(EXIT_FAILURE);
     }
 }
 
