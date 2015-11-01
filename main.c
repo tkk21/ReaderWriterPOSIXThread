9b27e8a0c5a8eed7c61a5fc759db532aa0019aa3
tkk21 <tkk21@case.edu>
2015-10-31 23:33:15 -0400

    added semaphore functions

--- main.c
+++ main.c
@@ -7,4 +7,26 @@
 typedef struct _thread_data_ {
     int tid;// thread's id
     int readcount; // status of whether or not something was read or not
+} thread_data;
+
+// Error checked syscall of semaphore wait
+void wait(sem_t *sem){
+    if(sem_wait(sem) < 0) {
+        perror ("sem_wait");
+        exit (EXIT_FAILURE);
+    }
 }
+
+// Error checked syscall of semaphore signal
+void signal(sem_t *sem){
+    if(sem_post(sem) < 0){
+        perror ("sem_post");
+        exit (EXIT_FAILURE);
+    }
+}
+
+int main (){
+
+
+}
+
